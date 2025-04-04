0084b525b46382edee15a54aa290be7a28c08ccf
Fuzz Leonard <fuzz@starlight.local>
2025-04-03 22:41:04 -0700

    Initial commit

---
+++ lib/haskbrew.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "haskbrew/version"
+
+module Haskbrew
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/haskbrew/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Haskbrew
+  VERSION = "0.1.0"
+end
